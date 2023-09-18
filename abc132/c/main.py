1054c21033c0e94d192f5fa33f50e2e37e24cc21
Neru-K <atokoam@gmail.com>
2023-09-19 00:18:43 +0900

    added whitespace

--- abc132/c/main.py
+++ abc132/c/main.py
@@ -1,14 +1,14 @@
 N = int(input())
-D = list(map(int,input().split()))
+D = list(map(int, input().split()))
 
 sorted = sorted(D)
 
 count = 0
 
-if N % 2 == 0:
-    start = sorted[(N-1)//2]+1
-    end = sorted[(N+1)//2]+1
-    for i in range(start,end):
-        count += 1
+
+start = sorted[(N-1)//2]+1
+end = sorted[(N+1)//2]+1
+for i in range(start, end):
+    count += 1
 
 print(count)
