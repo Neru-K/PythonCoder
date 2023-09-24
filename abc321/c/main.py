b0ae69cc35168223d88687bd59143ed4208a0a74
Neru-K <atokoam@gmail.com>
2023-09-24 22:09:45 +0900

    submitted

---
+++ abc321/c/main.py
@@ -0,0 +1,13 @@
+K = int(input())
+
+i = 1
+while True:
+    s = str(i)
+
+    for j in range(len(s)):
+        for 
+            if i == K:
+                print(i)
+                break
+            else:
+                j += 1
---
+++ tenka1-2017-beginner/a/main.py
@@ -0,0 +1,9 @@
+S = input()
+
+count = 0
+
+for c in S:
+    if c == "1":
+        count += 1
+
+print(count)
