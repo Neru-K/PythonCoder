18f13b0aa4d31d448180c9850d9154e24932cf9e
Neru-K <atokoam@gmail.com>
2023-10-14 20:56:52 +0900

    submitted

--- abc154/c/main.py
+++ abc154/c/main.py
@@ -1,10 +1,9 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-result = "YES"
-for i in range(N):
-    for j in range(i + 1, N):
-        if A[i] == A[j]:
-            result = "NO"
-            break
-print(result)
+unique = set(A)
+
+if len(unique) == N:
+    print("YES")
+else:
+    print("NO")
