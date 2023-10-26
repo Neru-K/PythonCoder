713703d30665545d89b2c071b023e22e383bfa78
Neru-K <atokoam@gmail.com>
2023-10-27 04:37:48 +0900

    submitted

--- abc325/c/main.py
+++ abc325/c/main.py
@@ -1,25 +1,21 @@
-# Union-Find 木
 class unionfind:
-    # n 頂点の Union-Find 木を作成
-    # （ここでは頂点番号が 1-indexed になるように実装しているが、0-indexed の場合は par, size のサイズは n でよい）
     def __init__(self, n):
         self.n = n
-        self.par = [-1] * (n + 1)  # 最初は親が無い
-        self.size = [1] * (n + 1)  # 最初はグループの頂点数が 1
+        self.par = [-1] * (n + 1)
+        self.size = [1] * (n + 1)
 
-    # 頂点 x の根を返す関数
     def root(self, x):
-        # 1 個先（親）がなくなる（つまり根に到達する）まで、1 個先（親）に進み続ける
-        while self.par[x] != -1:
-            x = self.par[x]
-        return x
+        if self.par[x] == -1:
+            return x
+        else:
+            # Path compression
+            self.par[x] = self.root(self.par[x])
+            return self.par[x]
 
-    # 要素 u, v を統合する関数
     def unite(self, u, v):
         rootu = self.root(u)
         rootv = self.root(v)
         if rootu != rootv:
-            # u と v が異なるグループのときのみ処理を行う
             if self.size[rootu] < self.size[rootv]:
                 self.par[rootu] = rootv
                 self.size[rootv] += self.size[rootu]
@@ -27,22 +23,36 @@ class unionfind:
                 self.par[rootv] = rootu
                 self.size[rootu] += self.size[rootv]
 
-    #  要素 u と v が同一のグループかどうかを返す関数
     def same(self, u, v):
         return self.root(u) == self.root(v)
 
 
-# 入力
-N, Q = map(int, input().split())
-queries = [list(map(int, input().split())) for i in range(Q)]
+def in_bound(r, c, H, W):
+    return 0 <= r < H and 0 <= c < W
 
-# クエリの処理
-uf = unionfind(N)
-for tp, u, v in queries:
-    if tp == 1:
-        uf.unite(u, v)
-    if tp == 2:
-        if uf.same(u, v):
-            print("Yes")
-        else:
-            print("No")
+
+# 入力
+H, W = map(int, input().split())
+grid_nums = H * W
+uf = unionfind(grid_nums)
+directions = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
+
+count = 0
+
+S = [input() for _ in range(H)]
+
+for i in range(grid_nums):
+    r = i // W
+    c = i % W
+    if S[r][c] == "#":
+        for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+            if in_bound(nr, nc, H, W) and S[nr][nc] == "#":
+                uf.unite(i, nr * W + nc)
+
+count = set()
+for j in range(grid_nums):
+    if S[j // W][j % W] == "#":
+        count.add(uf.root(j))
+
+print(len(count))
