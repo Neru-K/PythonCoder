d792e3a02058de08076439fb00ae62b4773288da
Neru-K <atokoam@gmail.com>
2023-10-31 01:40:41 +0900

    submitted

--- abc326/b/main.py
+++ abc326/b/main.py
@@ -1,21 +1,10 @@
-N = input()
+N = int(input())
 
-flag = False
+for num in range(N, 1000):
+    h = num // 100
+    t = (num // 10) % 10
+    o = num % 10
 
-for n in range(int(N[0]), 10):
-    for i in range(0, 10 - int(N[1])):
-        for j in range(0, 10):
-            if int(str(n) + str(i) + str(j)) < int(N):
-                continue
-
-            num = int(n) * i == j
-
-            if num:
-                print(str(n) + str(i) + str(j))
-                flag = True
-                break
-
-        if flag:
-            break
-    if flag:
+    if h * t == o:
+        print(num)
         break
