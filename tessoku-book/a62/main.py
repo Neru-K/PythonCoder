88fd4538e7d49bb90e35eb7e4725ee2339aa7dbc
Neru-K <atokoam@gmail.com>
2023-11-22 20:43:25 +0900

    submitted

---
+++ tessoku-book/a62/main.py
@@ -0,0 +1,32 @@
+import sys
+
+sys.setrecursionlimit(120000)
+
+
+def dfs(pos, G, visited):
+    visited[pos] = True
+    for i in G[pos]:
+        if not visited[i]:
+            dfs(i, G, visited)
+
+
+N, M = map(int, input().split())
+edges = [list(map(int, input().split())) for i in range(M)]
+
+G = [list() for i in range(N + 1)]
+for a, b in edges:
+    G[a].append(b)
+    G[b].append(a)
+
+visited = [False] * (N + 1)
+dfs(1, G, visited)
+
+answer = True
+for i in range(1, N + 1):
+    if visited[i] is False:
+        answer = False
+
+if answer is True:
+    print("The graph is connected.")
+else:
+    print("The graph is not connected.")
