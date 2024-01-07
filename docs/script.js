31486a65ed4487fe27c59442d7164a97c2ac79ac
Neru-K <atokoam@gmail.com>
2024-01-07 14:50:07 +0900

    changed directory path

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,7 @@
 (function () {
     window.addEventListener('DOMContentLoaded', function () {
         // JSONファイルのURL
-        const jsonUrl = 'https://github.com/Neru-K/PythonCoder/blog/blog.json';
+        const jsonUrl = 'https://github.com/Neru-K/PythonCoder/docs/blog.json';
 
         // JSONデータを取得して表示
         fetch(jsonUrl)
