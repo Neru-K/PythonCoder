d7a8ec19f368679367604ebe176d4c243626255e
Neru-K <atokoam@gmail.com>
2024-05-01 02:31:49 +0900

    submitted

--- abc251/b/main.py
+++ abc251/b/main.py
@@ -1,14 +1,19 @@
+from itertools import combinations
+
 N,W = map(int,input().split())
-A = list(map(int,input().split()))
 
-#bit全探索の基本コード(itertools モジュール)
-from itertools import product
+mn = min(3, N)
+A = ([0] * mn) + list(map(int,input().split()))
 
+comb = combinations(A, mn)
 
-for pro in product((0, 1), repeat=N):
-    sum_p = sum(pro)
-    if sum_p > 0 and sum_p < 4:
-        sum_a = 0
-        for i in range(N):
-            if pro[i] == 1:
-                sum_a += A[pro[i]]
+set_n = set()
+count = 0
+
+for i in list(comb):
+    sm = sum(i)
+    if sm not in set_n and 0 < sm and sm <= W:
+        set_n.add(sm)
+        count += 1
+		
+print(count)
