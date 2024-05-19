1a84448b2d8f6c249e8c53b9b4127a85d780bfda
Neru-K <atokoam@gmail.com>
2024-05-20 03:08:44 +0900

    submitted

--- abc258/b/main.py
+++ abc258/b/main.py
@@ -1,43 +1,39 @@
 N = int(input())
-start = []
 A = []
 
 max_a = 0
 for i in range(N):
     A.append(input())
     for j in range(N):
-        if A[j] > max_a:
-            max_a = A[j]
+        if int(A[i][j]) > max_a:
+            max_a = int(A[i][j])
 
 dx = [0, 1, 1, 1, 0, -1, -1, -1]
 dy = [1, 1, 0, -1, -1, -1, 0, 1]
 
-visited = set()
-max_result = ""
+result = []
 
 
 def grid(y, x):
-    max = 0
-    max_ny = 0
-    max_nx = 0
     
     for i in range(8):
-        nx = (x + dx[i]) % N
-        ny = (y + dy[i]) % N
+        num = ""
+        for j in range(N):
+            nx = (x + (dx[i] * j)) % N
+            ny = (y + (dy[i] * j)) % N
 
-        if not (ny, nx) in visited:
-            visited.add((ny, nx))
-            if A[ny][nx] > max:
-                max = A[ny][nx]
-                max_ny = ny
-                max_nx = nx
+            num += A[ny][nx]
 
-    return (max_ny, max_nx)
+        result.append(num)
 
 
-returned_tuple = (0, 0)
+max_a = str(max_a)
 
 for y in range(N):
     for x in range(N):
         if A[y][x] == max_a:
-            returned_tuple = grid(y, x)
+            grid(y, x)
+
+result.sort(reverse=True)
+
+print(result[0])
