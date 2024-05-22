13d1cf28945e6171a161ff3eca2a63afcaeb6d16
Neru-K <atokoam@gmail.com>
2024-05-23 04:03:04 +0900

    submitted

--- abc259/b/main.py
+++ abc259/b/main.py
@@ -2,7 +2,9 @@ import math
 
 a, b, d = map(int, input().split())
 
-translateX = math.sqrt(a**2 + b**2) * math.cos(math.radians(d))
-translateY = math.sqrt(a**2 + b**2) * math.sin(math.radians(d))
+rad = math.radians(d)
+
+translateX = a * math.cos(math.radians(d)) - b * math.sin(math.radians(d))
+translateY = a * math.sin(math.radians(d)) + b * math.cos(math.radians(d))
 
 print(*[translateX, translateY])
