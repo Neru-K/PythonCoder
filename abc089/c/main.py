663401b351543f54d0644179f78fda3b5a6f570a
Neru-K <atokoam@gmail.com>
2024-12-02 22:47:01 +0900

    submitted

--- abc089/c/main.py
+++ abc089/c/main.py
@@ -2,15 +2,23 @@
 # それぞれの頭文字で何通りあるかを
 # あとから引き算？
 N = int(input())
+chars = "MARCH"
+c = [0] * 5
 
-dict = {"M": 0, "A": 0, "R": 0, "C": 0, "H": 0}
-
-for i in range(N):
+for _ in range(N):
     S = input()
     first_char = S[0]
 
-    if first_char in dict:
-        dict[first_char] += 1
+    for i in range(5):
+        if first_char == chars[i]:
+            c[i] += 1
+            break
 
-for c in dict:
-    print(dict[c])
+count = 0
+
+for j in range(5):
+    for k in range(j + 1, 5):
+        for l in range(k + 1, 5):
+            count += c[j] * c[k] * c[l]
+
+print(count)
