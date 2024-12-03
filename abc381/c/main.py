17b80dcbfda1dc6f1b55b5825af835902f79f314
Neru-K <atokoam@gmail.com>
2024-12-03 23:32:50 +0900

    not submitted

--- abc381/c/main.py
+++ abc381/c/main.py
@@ -7,21 +7,32 @@ cnt_two = 0
 cnt_one = 0
 cnt_slash = 0
 
-for i in range(N):
+if S[0] == "1":
+    cnt_one = 1
+elif S[0] == "2":
+    cnt_two = 1
+elif S[0] == "/":
+    cnt_slash = 1
+
+for i in range(1, N):
     Char = S[i]
+    Char_prev = S[i - 1]
     if Char == "1":
         cnt_one += 1
-        if i != N - 1:
-            if Char == "1" or Char == "/":
-                cnt_one = 0
 
     elif Char == "2":
         cnt_two += 1
 
-    elif Char == "/":
-        if cnt_slash == 0:
-            cnt_slash += 1
-        else:
-            max_count = max(max_count, 1)
+        if Char_prev == "1":
             cnt_one = 0
-            cnt_two = 0
+
+    elif Char == "/":
+        cnt_slash = 1
+        
+        if Char_prev == "2":
+            count = max(cnt_slash, cnt_one, cnt_two)
+            if count > max_count:
+                max_count = count
+            count = 0
+
+print(max_count)
