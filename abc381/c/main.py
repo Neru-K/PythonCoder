664c8b26e9a8877c72d503c5795601b71491cc32
Neru-K <atokoam@gmail.com>
2024-12-04 11:01:41 +0900

    submitted

--- abc381/c/main.py
+++ abc381/c/main.py
@@ -15,24 +15,40 @@ elif S[0] == "/":
     cnt_slash = 1
 
 for i in range(1, N):
-    Char = S[i]
-    Char_prev = S[i - 1]
-    if Char == "1":
+    char = S[i]
+    char_prev = S[i - 1]
+
+    if char_prev == "2" and char != "2" and cnt_slash == 1:
+        count = cnt_slash + (min(cnt_one, cnt_two) * 2)
+        if count > max_count:
+            max_count = count
+
+    elif (char == "1" and char_prev == "/") or (char == "/" and char_prev == "/"):
+        cnt_one = 0
+        cnt_two = 0
+        if cnt_slash > max_count:
+            max_count = cnt_slash
+
+    if char == "1":
         cnt_one += 1
 
-    elif Char == "2":
+        if char_prev == "2":
+            cnt_two = 0
+
+        elif char_prev == "/":
+            cnt_slash = 0
+
+    elif char == "2":
         cnt_two += 1
 
-        if Char_prev == "1":
+        if char_prev == "1":
             cnt_one = 0
 
-    elif Char == "/":
+    elif char == "/":
         cnt_slash = 1
-        
-        if Char_prev == "2":
-            count = max(cnt_slash, cnt_one, cnt_two)
-            if count > max_count:
-                max_count = count
-            count = 0
+
+        if char_prev == "2":
+            cnt_two = 0
+            cnt_one = 0
 
 print(max_count)
