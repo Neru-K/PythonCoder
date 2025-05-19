7cf2c6387faac9ea6ff75bc11efc3dc1ca9963f2
Neru-K <atokoam@gmail.com>
2025-05-19 16:43:38 +0900

    submitted

--- abc394/c/main.py
+++ abc394/c/main.py
@@ -3,85 +3,9 @@ import re
 S = input()
 result_s = list(S)
 
-pattern = r"W+A"
-
-for match in re.finditer(pattern, S):
-    result_s[match.regs[0][0]] = "A"
-    for l in range(match.regs[0][0] + 1, match.regs[0][1]):
-        result_s[l] = "C"
+for i in range(len(S) - 1, 0, -1):
+    if "".join(result_s[i - 1 : i + 1]) == "WA":
+        result_s[i - 1] = "A"
+        result_s[i] = "C"
 
 print(*result_s, sep="")
-
-arr_pos = []
-result_s = list(S)
-
-"""
-条件分岐
-
-・最初の１文字目の場合
-  Aの場合
-    lastidxを更新
-  それ以外
-    特になし
-
-・2文字目以降の場合
-  ・Aの場合
-    lastidxを更新
-    firstidxをリセット
-
-  ・Wの場合
-    前の文字がWの場合
-      firstidxを更新
-    前の文字がAの場合
-      firstidxを更新
-
-  ・それ以外
-    lastidx, firstidxが-1じゃない場合、配列に追加
-    lastidx, firstidxをリセット
-
-
-"""
-
-""" firstidx = -1
-lastidx = -1
-flag = False
-
-for i in range(len(S) - 1, -1, -1):
-    if i == len(S) - 1:
-        if S[i] == "A":
-            lastidx = i
-
-        continue
-
-    if S[i] == "A":
-        if lastidx != -1 and firstidx != -1:
-            arr_pos.append((firstidx, lastidx))
-
-        lastidx = i
-        firstidx = -1
-
-    elif S[i] == "W":
-        if S[i + 1] == "A":
-            firstidx = i
-
-        elif S[i + 1] == "W":
-            firstidx = i
-
-    else:
-        if lastidx != -1 and firstidx != -1:
-            arr_pos.append((firstidx, lastidx))
-            lastidx = -1
-            firstidx = -1
-
-    if i == 0 and S[i] == "W" and lastidx != -1 and firstidx != -1:
-        arr_pos.append((firstidx, lastidx))
-        lastidx = -1
-        firstidx = -1
-
-for pair in arr_pos:
-    result_s[pair[0]] = "A"
-    for l in range(pair[0] + 1, pair[1] + 1):
-        result_s[l] = "C"
-
-print(*result_s, sep="")
- """
