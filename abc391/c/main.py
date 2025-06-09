a07ad9a2b9606dbc866cf73d407eb8f212c53041
Neru-K <atokoam@gmail.com>
2025-06-09 16:58:02 +0900

    submitted

--- abc391/c/main.py
+++ abc391/c/main.py
@@ -6,18 +6,24 @@ position_p = []
 for p in range(N):
     position_p.append(p)
 
+ans = 0
+
 for i in range(Q):
     q = list(map(int, input().split()))
     if q[0] == 1:
-        P, H = q[1], q[2]
-        # 今いる巣を取り出す。
-        position_p[P - 1] = H - 1
-        nest[P - 1] -= 1
-        nest[H - 1] += 1
+        P, H = q[1], q[2]  # クエリ。鳩Pを巣Hに移動させる。
+        position_before = position_p[P - 1]  # 鳩Pが今いる巣を取り出す。
+        position_p[P - 1] = H - 1  # 鳩Pを巣Hに移動させる。
+
+        nest[position_before - 1] -= 1  # 移動元の巣の数を減らす
+        if nest[position_before - 1] == 1:
+            ans -= 1
+        nest[H - 1] += 1  # 移動先の巣の数を増やす
+        if nest[H - 1] == 2:
+            ans += 1
 
         # ansを持っておいて、クエリ1の時には更新しておく。クエリ2でそれを出力するだけ。
-
         # 巣にいる鳩が1匹から2匹になったタイミング、もしくは2匹が1匹になったタイミングが鍵。
 
     else:
-        print()
+        print(ans)
