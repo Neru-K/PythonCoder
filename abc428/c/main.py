35bdc9faead8d259fcda9e1c218fd9cb9ec42c36
Neru-K <atokoam@gmail.com>
2025-11-03 23:03:00 +0900

    submitted

--- abc428/c/main.py
+++ abc428/c/main.py
@@ -1,13 +1,42 @@
 Q = int(input())
 
-count_L = 0
-count_R = 0
+list_br = []
+countL = 0
+countR = 0
+len_yes = 0
+is_bad = False
 
-for i in range(1, Q):
+for i in range(Q):
     q, *c = map(str, input().split())
 
+    if q == "1":
+        if c[0] == "(":
+            countL += 1
+        else:
+            countR += 1
+        list_br.append(c[0])
+    else:
+        char = list_br.pop()
+        if char == "(":
+            countL -= 1
+        else:
+            countR -= 1
+
+        if len(list_br) == len_yes:
+            is_bad = False
+
+    if countR > countL:
+        is_bad = True
+        len_yes = len(list_br) - 1
+
+    if countL == countR and not is_bad:
+        print("Yes")
+    else:
+        print("No")
+
+
 # 条件1　LとRの数が同じ
-# 条件2　1度L>Rになってしまったら以降は悪い括弧列になる
+# 条件2　1度L<Rになってしまったら以降は悪い括弧列になる
 
 # 条件1を記録するには・・・countL, countRがあれば良い
 # 条件2を記録するには・・・フラグがあれば良い
