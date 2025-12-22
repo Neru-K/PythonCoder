fa9a2b00f49559e4e5a46b80a81f5ec1cdad2a5d
Neru-K <atokoam@gmail.com>
2025-12-22 22:34:49 +0900

    submitted

--- abc436/c/main.py
+++ abc436/c/main.py
@@ -1,15 +1,26 @@
 N, M = map(int, input().split())
 
-exist = set()
+set_grid = set()
+direction = [(0, 0), (0, 1), (1, 1), (1, 0)]
 
 ans = 0
 
+
+def ok(R, C):
+    for j in range(4):
+        now = (R + direction[j][0], C + direction[j][1])
+        if now in set_grid:
+            return False
+
+    return True
+
+
 for i in range(M):
     R, C = map(int, input().split())
-    R2, C2 = (R + 1) // 2, (C + 1) // 2
 
-    if not (R2, C2) in exist:
+    if ok(R, C):
+        for k in range(4):
+            set_grid.add((R + direction[k][0], C + direction[k][1]))
         ans += 1
-        exist.add((R2, C2))
 
 print(ans)
