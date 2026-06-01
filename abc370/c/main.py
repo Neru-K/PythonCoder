788240ac6d1abe7d22bd9e65582f2345fd6297e6
Neru-K <atokoam@gmail.com>
2026-06-01 20:56:35 +0900

    submitted

--- abc370/c/main.py
+++ abc370/c/main.py
@@ -1,34 +1,23 @@
-S = input()
-T = input()
-list_s = []
-list_t = []
-list_diff = []
-list_i = []
+S = list(input())
+T = list(input())
 X = []
 
-if S == T:
-    print(0)
-    exit()
-
 for i in range(len(S)):
-    ord_s = ord(S[i]) - 96
-    ord_t = ord(T[i]) - 96
-    diff = 26 * (len(T) - i) + (ord_t - ord_s)
+    s = S[i]
+    t = T[i]
+    if ord(s) > ord(t):
+        S[i] = t
+        X.append("".join(S))
 
-    list_diff.append(diff)
-    list_i.append(i)
+for j in range(len(S) - 1, -1, -1):
+    s = S[j]
+    t = T[j]
+    if ord(s) < ord(t):
+        S[j] = t
 
-sorted_pairs = sorted(zip(list_diff, list_i))
-
-for j in range(len(S)):
-    idx = sorted_pairs[j][1]
-
-    if S[idx] != T[idx]:
-        S = S[0:idx] + T[idx] + S[idx+1:]
-        X.append(S)
-
+        X.append("".join(S))
 
 print(len(X))
-for k in range(len(X)):
 
-    print(X[k])
+for x in X:
+    print(x)
